ad5b149493640127ae4be484f67832d902ce7d20
BuildTools <theonlygusti@gmail.com>
2017-01-02 21:17:47 +0000

    Skeleton file, listens for onEnable but does nothing else yet. Extends JavaPlugin.

---
+++ src/main/java/io/github/theonlygusti/spellcastr/Plugin.java
@@ -0,0 +1,26 @@
+/*
+ *  This file is part of SpellCastr.
+ *
+ *  SpellCastr is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SpellCastr is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SpellCastr  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.theonlygusti.spellcastr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugin extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    getLogger().info("Enabled.");
+  }
+}
